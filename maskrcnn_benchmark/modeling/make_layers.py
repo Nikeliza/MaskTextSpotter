d1a02d4c2d5111e7846c53227353a84f8588aaf5
Nikeliza <nikeliza@mail.ru>
2021-01-13 19:50:04 +0300

    Create make_layers.py

---
+++ maskrcnn_benchmark/modeling/make_layers.py
@@ -0,0 +1,192 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+"""
+Miscellaneous utility functions
+"""
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from maskrcnn_benchmark.config import cfg
+from maskrcnn_benchmark.layers import Conv2d
+from maskrcnn_benchmark.modeling.poolers import Pooler
+
+
+def get_group_gn(dim, dim_per_gp, num_groups):
+    """get number of groups used by GroupNorm, based on number of channels."""
+    assert dim_per_gp == -1 or num_groups == -1, \
+        "GroupNorm: can only specify G or C/G."
+
+    if dim_per_gp > 0:
+        assert dim % dim_per_gp == 0, \
+            "dim: {}, dim_per_gp: {}".format(dim, dim_per_gp)
+        group_gn = dim // dim_per_gp
+    else:
+        assert dim % num_groups == 0, \
+            "dim: {}, num_groups: {}".format(dim, num_groups)
+        group_gn = num_groups
+
+    return group_gn
+
+
+def group_norm(out_channels, affine=True, divisor=1):
+    out_channels = out_channels // divisor
+    dim_per_gp = cfg.MODEL.GROUP_NORM.DIM_PER_GP // divisor
+    num_groups = cfg.MODEL.GROUP_NORM.NUM_GROUPS // divisor
+    eps = cfg.MODEL.GROUP_NORM.EPSILON # default: 1e-5
+    return torch.nn.GroupNorm(
+        get_group_gn(out_channels, dim_per_gp, num_groups),
+        out_channels,
+        eps,
+        affine
+    )
+
+
+def make_conv3x3(
+    in_channels,
+    out_channels,
+    dilation=1,
+    stride=1,
+    use_gn=False,
+    use_relu=False,
+    kaiming_init=True
+):
+    conv = Conv2d(
+        in_channels,
+        out_channels,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        dilation=dilation,
+        bias=False if use_gn else True
+    )
+    if kaiming_init:
+        nn.init.kaiming_normal_(
+            conv.weight, mode="fan_out", nonlinearity="relu"
+        )
+    else:
+        torch.nn.init.normal_(conv.weight, std=0.01)
+    if not use_gn:
+        nn.init.constant_(conv.bias, 0)
+    module = [conv,]
+    if use_gn:
+        module.append(group_norm(out_channels))
+    if use_relu:
+        module.append(nn.ReLU(inplace=True))
+    if len(module) > 1:
+        return nn.Sequential(*module)
+    return conv
+
+
+def make_fc(dim_in, hidden_dim, use_gn=False):
+    '''
+        Caffe2 implementation uses XavierFill, which in fact
+        corresponds to kaiming_uniform_ in PyTorch
+    '''
+    if use_gn:
+        fc = nn.Linear(dim_in, hidden_dim, bias=False)
+        nn.init.kaiming_uniform_(fc.weight, a=1)
+        return nn.Sequential(fc, group_norm(hidden_dim))
+    fc = nn.Linear(dim_in, hidden_dim)
+    nn.init.kaiming_uniform_(fc.weight, a=1)
+    nn.init.constant_(fc.bias, 0)
+    return fc
+
+
+def conv_with_kaiming_uniform(use_gn=False, use_relu=False):
+    def make_conv(
+        in_channels, out_channels, kernel_size, stride=1, dilation=1
+    ):
+        conv = Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=dilation * (kernel_size - 1) // 2,
+            dilation=dilation,
+            bias=False if use_gn else True
+        )
+        # Caffe2 implementation uses XavierFill, which in fact
+        # corresponds to kaiming_uniform_ in PyTorch
+        nn.init.kaiming_uniform_(conv.weight, a=1)
+        if not use_gn:
+            nn.init.constant_(conv.bias, 0)
+        module = [conv,]
+        if use_gn:
+            module.append(group_norm(out_channels))
+        if use_relu:
+            module.append(nn.ReLU(inplace=True))
+        if len(module) > 1:
+            return nn.Sequential(*module)
+        return conv
+
+    return make_conv
+
+# TODO:下面的为mobilenetV2的层
+class InvertedResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, expansion_factor=6, kernel_size=3, stride=2):
+        super(InvertedResidualBlock, self).__init__()
+
+        if stride != 1 and stride != 2:
+            raise ValueError("Stride should be 1 or 2")
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels * expansion_factor, 1, bias=False),
+            nn.BatchNorm2d(in_channels * expansion_factor),
+            # nn.ReLU6(inplace=True),
+            nn.ReLU6(),
+
+            nn.Conv2d(in_channels * expansion_factor, in_channels * expansion_factor,
+                      kernel_size, stride, 1,
+                      groups=in_channels * expansion_factor, bias=False),
+            nn.BatchNorm2d(in_channels * expansion_factor),
+            # nn.ReLU6(inplace=True),
+            nn.ReLU6(),
+
+            nn.Conv2d(in_channels * expansion_factor, out_channels, 1,
+                      bias=False),
+            nn.BatchNorm2d(out_channels))
+
+        self.is_residual = True if stride == 1 else False
+        self.is_conv_res = False if in_channels == out_channels else True
+
+        # Assumption based on previous ResNet papers: If the number of filters doesn't match,
+        # there should be a conv1x1 operation.
+        if stride == 1 and self.is_conv_res:
+            self.conv_res = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
+                                          nn.BatchNorm2d(out_channels))
+
+    def forward(self, x):
+        block = self.block(x)
+        if self.is_residual:
+            if self.is_conv_res:
+                return self.conv_res(x) + block
+            return x + block
+        return block
+    
+def inverted_residual_sequence(in_channels, out_channels, num_units, expansion_factor=6,
+                               kernel_size=3,
+                               initial_stride=2):
+    bottleneck_arr = [
+        InvertedResidualBlock(in_channels, out_channels, expansion_factor, kernel_size,
+                              initial_stride)]
+
+    for i in range(num_units - 1):
+        bottleneck_arr.append(
+            InvertedResidualBlock(out_channels, out_channels, expansion_factor, kernel_size, 1))
+
+    return bottleneck_arr
+
+
+def conv2d_bn_relu6(in_channels, out_channels, kernel_size=3, stride=2, dropout_prob=0.0):
+    # To preserve the equation of padding. (k=1 maps to pad 0, k=3 maps to pad 1, k=5 maps to pad 2, etc.)
+    padding = (kernel_size + 1) // 2 - 1
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+        nn.BatchNorm2d(out_channels),
+        # For efficiency, Dropout is placed before Relu.
+        # nn.Dropout2d(dropout_prob, inplace=True),
+        nn.Dropout2d(dropout_prob),
+        # Assumption: Relu6 is used everywhere.
+        # nn.ReLU6(inplace=True),
+        nn.ReLU6()
+    )
